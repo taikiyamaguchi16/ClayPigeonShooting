c1b47e7446a2739ed63aecf149fd7d940ada5df7
tai <taikitpmiita@icloud.com>
2022-01-31 21:55:59 +0900

    [add]いろいろ

---
+++ Assets/yamaguchi/script/WaveController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+{
+    public List<float> shotTimes = new List<float>();
+
+    //二連続撃ちの回数
+    public int doubuleShotNum;
+    //三連続うちの回数
+    public int tripleShotNum;
+
+    private float elpsedTime;
+
+    private int shotCount;
+
+    public WaveManager myManager;
+    private void Start()
+    {
+        elpsedTime = 0f;
+        shotCount = 0;
+    }
+
+    public void ActiveWaveUpdate()
+    {
+        elpsedTime += Time.deltaTime;
+        if (shotCount < shotTimes.Count)
+        {
+            if (elpsedTime > shotTimes[shotCount])
+            {
+                Debug.Log(this.gameObject.name+"弾を発射");
+                elpsedTime = 0f;
+                shotCount++;
+            }
+        }
+        else
+        {
+            StartCoroutine(myManager.FinishWave());
+        }
+    }
+}
---
+++ Assets/yamaguchi/script/WaveManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    private int waveCount;
+    public List<WaveController> wavesList = new List<WaveController>();
+
+    private bool changeNow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveCount = 0;
+        changeNow = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(waveCount<wavesList.Count)
+        {
+            if(!changeNow)
+                wavesList[waveCount].ActiveWaveUpdate();
+        }
+    }
+
+
+    public IEnumerator FinishWave()
+    {
+        //ここで少し待って切り替わり演出入れるといいかも
+        changeNow = true;
+        Debug.Log("waveを切り替えます");
+        yield return new WaitForSeconds(5f);
+        waveCount++;
+        Debug.Log("切り替わりました");
+        changeNow = false;
+        yield break;
+    }
+}
