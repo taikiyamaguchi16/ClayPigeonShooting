bfe7eebccc3f5032f9dd072dc03fdb1dc348f538
jasu_222 <justicekenta@i.softbank.jp>
2022-02-07 23:55:44 +0900

    [update]残弾管理追加

--- Assets/jasu/Sprite/VRGunController.cs
+++ Assets/jasu/Sprite/VRGunController.cs
@@ -13,23 +13,42 @@ public class VRGunController : MonoBehaviour
     [SerializeField]
     Transform leftHand;
 
+    public int ammoNum;
+
+    public int maxAmmo = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ammoNum = maxAmmo;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger) || Input.GetKeyDown(KeyCode.Return))
         {
-            bulletSpawner.Spawn();
+            if(ammoNum > 0)
+            {
+                bulletSpawner.Spawn();
+                ammoNum--;
+            }
+            else
+            {
+
+            }
         }
 
+        // 向き
         transform.position = rightHand.position;
 
         Vector3 lookVec = leftHand.position - transform.position;
         transform.rotation = Quaternion.LookRotation(lookVec);
+    }
+
+    public void ResetAmmo(int _maxAmmo)
+    {
+        maxAmmo = _maxAmmo;
+        ammoNum = maxAmmo;
     }
 }
